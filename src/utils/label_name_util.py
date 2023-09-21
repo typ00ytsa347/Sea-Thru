6a792c1283162782c1bbf71f3750864c6a1ba57a
Yun-Shan Tsai <ytsa347@aucklanduni.ac.nz>
2023-09-21 15:53:39 +1200

    import names for the classes

--- src/utils/label_name_util.py
+++ src/utils/label_name_util.py
@@ -2,5 +2,16 @@ import os
 import shutil
 
 src_dir = "/home/anyone/Downloads/IMAGENET.md"
+dest_dir = "/home/anyone/Desktop/labelled_names.txt"
 
-labelled_names = open("")
+src_format = open(src_dir, "r")
+lines = src_format.readlines()
+
+dest_format = open(dest_dir, "w")
+
+for line in lines:
+    split_line = line.split("|")
+    dest_format.write(split_line[1].strip() + ": " + split_line[2].strip() + "\n")
+
+src_format.close()
+dest_format.close()
