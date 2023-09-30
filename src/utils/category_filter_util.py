3e3e86d5f4b71bf93f52b58dfefaa8054b5b70bb
Yun-Shan Tsai <ytsa347@aucklanduni.ac.nz>
2023-09-30 13:59:05 +1300

    29Sep run results

--- src/utils/category_filter_util.py
+++ src/utils/category_filter_util.py
@@ -1,11 +1,11 @@
 import os
 import shutil, errno
 
-src_path = '/home/anyone/Desktop/data_failsafe/images_unfoldered_filtered'
-dest_path = '/home/anyone/Desktop/data_failsafe/val_images_filtered'
+src_path = '/home/anyone/Desktop/data_failsafe/turb/images_foldered'
+dest_path = '/home/anyone/Desktop/data_failsafe/zmurez/restoration/train_images_filtered'
 
-label_src_path = '/home/anyone/Desktop/data_failsafe/val_labels_txt'
-label_dest_path = '/home/anyone/Desktop/data_failsafe/val_labels_filtered_txt'
+label_src_path = '/home/anyone/Desktop/data_failsafe/turb/val_labels_txt'
+label_dest_path = '/home/anyone/Desktop/data_failsafe/turb/val_labels_filtered_txt'
 
 category_dict = dict()
 name_list = list()
@@ -23,17 +23,18 @@ with open('./res/aquatic_category_list.txt') as aquatic_category_list:
         key = int(parts[0])
         name_list.append(category_dict[key])
 
-for filename in os.listdir(src_path):
-    category = filename.split("_")[0]
-    if name_list.__contains__(category):
-        src = os.path.join(src_path, filename)
-        dst = os.path.join(dest_path, filename)
-        try:
-            shutil.copy(src, dst)
-        except OSError as exc: # python >2.5
-            if exc.errno in (errno.ENOTDIR, errno.EINVAL):
+for category in os.listdir(src_path):
+    for filename in os.listdir(os.path.join(src_path, category)):
+        # category = filename.split("_")[0]
+        if category not in name_list:
+            src = os.path.join(src_path, category, filename)
+            dst = os.path.join(dest_path, filename)
+            try:
                 shutil.copy(src, dst)
-            else: raise
+            except OSError as exc: # python >2.5
+                if exc.errno in (errno.ENOTDIR, errno.EINVAL):
+                    shutil.copy(src, dst)
+                else: raise
 
 for filename in os.listdir(label_src_path):
     category = filename.split("_")[0]
